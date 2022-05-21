0382140ee4227b5ce939373e528560730a5e4fb6
iazadur <iamazadur@gmail.com>
2022-05-22 01:35:57 +0600

    login with home page some from complete

---
+++ js/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+	theme: {
+		extend: {},
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		container: {
+			center: true,
+		},
+	},
+}
+
